0798983e379bd370574b724bed92d71e12d1c93a
Manuela Gomez <manuelagomezmgg@gmail.com>
2024-09-18 23:34:04 -0600

    repaso clase 2

---
+++ repasoClase2/js/index.js
@@ -0,0 +1,139 @@
+/* 
+
+EJERCICIO 1
+
+
+Realizá un programa que permita ingresar 3 notas pertenecientes  para cierto alumno de nivel secundario. Debe calcularse y mostrarse la nota promedio.
+
+let nota1 = parseInt(prompt("Ingresa la primera nota"));
+let nota2 = parseInt(prompt("Ingresa la segunda nota"));
+let nota3 = parseInt(prompt("Ingresa la tercera nota"));
+
+let suma = (nota1 + nota2 + nota3)/3;
+let promedio = suma/3
+
+console.log("El promedio: " + promedio.toFixed(2));
+
+
+----------
+
+
+EJERCICIO 2
+
+
+Realizá un programa que permita ingresar un número entero. Debe mostrarse el número ingresado: a. Multiplicado por 5. b. Dividido por 2.
+
+
+Realizar un programa q permita ingresar un numero entero. debe mostrarse el numero ingresado. multiplicarlo por 5 y luego dividirlo entre 2.
+
+let numeroEntero = parseInt(prompt("ingresa tu numero: "));
+
+console.log(numeroEntero);
+
+let multiplicar = numeroEntero * 5;
+
+console.log(multiplicar);
+
+let dividir = multiplicar / 2;
+
+console.log("el resultado que me pediste es : " + dividir);
+
+
+
+---------
+
+
+/*
+
+EJERCICIO 3
+
+
+Realizá un programa que permita ingresar el valor monetario de una hora de trabajo y la cantidad de horas trabajadas por día por un trabajador. Debes mostrar el valor del salario semanal, sabiendo que trabaja todos los días hábiles y la mitad de las horas del día hábil los sábados. (Todas las horas valen lo mismo.) 
+
+
+
+
+
+
+X SEMANA
+
+let pagoHora = parseInt(prompt("Ingresa tu pago por hora"));
+
+console.log(pagoHora);
+
+let horasTrabajadas = 44;
+
+console.log(horasTrabajadas);
+
+let multiplicar = pagoHora * horasTrabajadas;
+
+console.log("Tu pago semanal es de " + multiplicar + " pesos.");
+
+
+
+
+
+X HRAS TRABAJADAS
+
+let pagoHora = parseInt(prompt("Ingresa tu pago por hora"));
+
+console.log(pagoHora);
+
+let horasTrabajadas = parseInt(prompt("Ingresa las horas trabajadas"));
+
+console.log(horasTrabajadas);
+
+let multiplicar = pagoHora * horasTrabajadas;
+
+console.log("Tu pago por las horas trabajadas es de " + multiplicar + " pesos.");
+
+
+
+----------
+
+
+
+ */
+
+
+
+/*
+
+
+
+EJERCICIO 4
+
+Realizá un programa que permita ingresar valores del mismo tipo para las variables num1 y num2. Una vez cargadas, mostrar ambas variables por pantalla, intercambiá sus valores (que lo cargado en num1 quede en num2, y viceversa) y volvé a mostrarlas actualizadas. 
+
+
+
+
+let num1 = prompt("Ingresa tu primer numero:");
+
+let num2 =prompt("Ingresa tu segundo numero:");
+
+console.log("Tu primer numero ingresado es " + num1 + " y tu segundo numero ingresado es " + num2);
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
